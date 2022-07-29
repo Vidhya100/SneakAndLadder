4b0ddff12b1e76dd0e1a1e1d9152ba26c9d13764
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-29 19:26:50 +0530

    Play or not added

--- CheckPlayOrNo.cs
+++ CheckPlayOrNo.cs
@@ -8,15 +8,55 @@ namespace SneakAndLadder
 {
     public static class CheckPlayOrNo
     {
+        const int NO_PLAY = 0;
+        const int PLAY=1;
+        const int SNEAK = 3;
+        const int LADDER = 5;
+         
+
         public static void CheckSneakOrLadder()
         {
             int startPoint = 0;
+            int ladderEnd = 22;
 
             Random random = new Random();
-            int roll = (random.Next(1, 6) + 1);
-            Console.WriteLine("roll : " + roll);
+            int check = random.Next(2);
+            Console.WriteLine("Got : " + check);
+           
+                switch (check)
+                {
+                    case NO_PLAY:
+                        startPoint = 0;
+                        Console.WriteLine("You got 0, so position: " + startPoint);
+                        break;
+                    case PLAY:
 
-            
+                        int roll = (random.Next(1, 6) + 1);
+                        Console.WriteLine("roll : " + roll);
+                        switch (roll)
+                        {
+                            case SNEAK:                                
+                                    startPoint -= roll;
+                                    Console.WriteLine("You got a sneak, so your position will be same a before."+startPoint);
+                                
+                                break;
+
+                            case LADDER:
+                                startPoint = ladderEnd + roll;
+                                Console.WriteLine("You got a ladder, so your new poition: "+startPoint);
+                                break;
+
+                            default:
+                                startPoint += roll;
+                            Console.WriteLine("Your position: " + startPoint);
+                                if (roll == 6)
+                                    Console.WriteLine("You got 6 so, play again.");
+                                break;
+                        }
+                        break;
+                
+
+            }            
         }
     }
 }
