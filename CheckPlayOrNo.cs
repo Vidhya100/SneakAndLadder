17825c80facc1d8acfb1431ce2b23ead7c667190
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-29 17:28:57 +0530

    Roll of 1 to 6

---
+++ CheckPlayOrNo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakAndLadder
+{
+    public static class CheckPlayOrNo
+    {
+        public static void CheckSneakOrLadder()
+        {
+            int startPoint = 0;
+
+            Random random = new Random();
+            int roll = (random.Next(1, 6) + 1);
+            Console.WriteLine("roll : " + roll);
+
+            
+        }
+    }
+}
